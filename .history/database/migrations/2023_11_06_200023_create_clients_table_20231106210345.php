3c0aa6811ed5b81f6f118996b9a0b6c16f84c5b9
hamzahbiri <hbiri.hamza@iit.ens.tn>
2023-11-06 21:10:40 +0100

    test

---
+++ .history/database/migrations/2023_11_06_200023_create_clients_table_20231106210345.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("Nom",50);
+            $table->string("Prenom",50);
+            $table->string("Email",100)->unique();
+            $table->string("Phone",20)->unique();
+            $table->string("vile",50);
+            $table->string("Raisoc",200);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
